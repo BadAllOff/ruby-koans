741a6c64c3b15759e02e7145f504b9b8775d8b07
Shamil <badalloffsh@gmail.com>
2017-11-05 14:07:20 +0100

    Add: about symbols complete

--- about_symbols.rb
+++ about_symbols.rb
@@ -26,7 +26,7 @@ class AboutSymbols < Neo::Koan
 
   def test_method_names_become_symbols
     symbols_as_strings = Symbol.all_symbols.map { |x| x.to_s }
-    assert_equal false, symbols_as_strings.include?("test_method_names_become_symbols")
+    assert_equal true, symbols_as_strings.include?("test_method_names_become_symbols")
   end
 
   # THINK ABOUT IT:
@@ -57,45 +57,71 @@ and "pollute" your list with the symbol you're testing for existence.
     def test_constants_become_symbols
       all_symbols_as_strings = Symbol.all_symbols.map { |x| x.to_s }
 
-      assert_equal __, all_symbols_as_strings.include?(RubyConstant)
+      assert_equal false, all_symbols_as_strings.include?(RubyConstant)
     end
   end
 
   def test_symbols_can_be_made_from_strings
     string = "catsAndDogs"
-    assert_equal __, string.to_sym
+    assert_equal :catsAndDogs, string.to_sym
   end
 
   def test_symbols_with_spaces_can_be_built
     symbol = :"cats and dogs"
 
-    assert_equal __.to_sym, symbol
+    assert_equal :"cats and dogs".to_sym, symbol
   end
 
   def test_symbols_with_interpolation_can_be_built
     value = "and"
     symbol = :"cats #{value} dogs"
 
-    assert_equal __.to_sym, symbol
+    assert_equal :"cats and dogs".to_sym, symbol
   end
 
   def test_to_s_is_called_on_interpolated_symbols
     symbol = :cats
     string = "It is raining #{symbol} and dogs."
 
-    assert_equal __, string
+    assert_equal "It is raining cats and dogs.", string
   end
 
   def test_symbols_are_not_strings
     symbol = :ruby
-    assert_equal __, symbol.is_a?(String)
-    assert_equal __, symbol.eql?("ruby")
+    assert_equal false, symbol.is_a?(String)
+    assert_equal false, symbol.eql?("ruby")
   end
+=begin 
+
+"cat".equal? "cat"      # false
+"cat" == "cat"          # true
+"cat".eql? "cat"        # true
+
+1 == 1                  # true
+1.eql? 1                # true
+1 == 1.0                # true
+1.eql? 1.0              # false
+1.0.eql? 1.0            # true
+
+What’s not immediately obvious is why are there two different methods 
+that seem to be doing exactly the same thing. 
+The answer is simple – eql? is meant to be used as a stricter version of ==, 
+if there is a need for such stricter version.eql? most prominent usage 
+is probably in the Hash class, where it’s used to test members for equality.
+
+In the Object class eql? is synonym with ==. 
+Most subclasses continue this tradition, 
+but there are a few classes that provide a different implementation for eql?. 
+Numeric types, for example, perform type conversion across ==, 
+but not across eql?, so:
+
+
+=end
 
   def test_symbols_do_not_have_string_methods
     symbol = :not_a_string
-    assert_equal __, symbol.respond_to?(:each_char)
-    assert_equal __, symbol.respond_to?(:reverse)
+    assert_equal false, symbol.respond_to?(:each_char)
+    assert_equal false, symbol.respond_to?(:reverse)
   end
 
   # It's important to realize that symbols are not "immutable
@@ -104,13 +130,13 @@ and "pollute" your list with the symbol you're testing for existence.
 
   def test_symbols_cannot_be_concatenated
     # Exceptions will be pondered further down the path
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       :cats + :dogs
     end
   end
 
   def test_symbols_can_be_dynamically_created
-    assert_equal __, ("cats" + "dogs").to_sym
+    assert_equal :catsdogs, ("cats" + "dogs").to_sym
   end
 
   # THINK ABOUT IT:
