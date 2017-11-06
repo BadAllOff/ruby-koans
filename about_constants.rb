c4e0db6cea5ed6ae21bc0be83aae945ba56baf99
Shamil <badalloffsh@gmail.com>
2017-11-06 12:46:53 +0100

    Add: about constants complete

--- about_constants.rb
+++ about_constants.rb
@@ -7,16 +7,16 @@ class AboutConstants < Neo::Koan
   C = "nested"
 
   def test_nested_constants_may_also_be_referenced_with_relative_paths
-    assert_equal __, C
+    assert_equal "nested", C
   end
 
   def test_top_level_constants_are_referenced_by_double_colons
-    assert_equal __, ::C
+    assert_equal "top level", ::C
   end
 
   def test_nested_constants_are_referenced_by_their_complete_path
-    assert_equal __, AboutConstants::C
-    assert_equal __, ::AboutConstants::C
+    assert_equal "nested", AboutConstants::C
+    assert_equal "nested", ::AboutConstants::C
   end
 
   # ------------------------------------------------------------------
@@ -35,7 +35,7 @@ class AboutConstants < Neo::Koan
   end
 
   def test_nested_classes_inherit_constants_from_enclosing_classes
-    assert_equal __, Animal::NestedAnimal.new.legs_in_nested_animal
+    assert_equal 4, Animal::NestedAnimal.new.legs_in_nested_animal
   end
 
   # ------------------------------------------------------------------
@@ -47,7 +47,7 @@ class AboutConstants < Neo::Koan
   end
 
   def test_subclasses_inherit_constants_from_parent_classes
-    assert_equal __, Reptile.new.legs_in_reptile
+    assert_equal 4, Reptile.new.legs_in_reptile
   end
 
   # ------------------------------------------------------------------
@@ -63,11 +63,13 @@ class AboutConstants < Neo::Koan
   end
 
   def test_who_wins_with_both_nested_and_inherited_constants
-    assert_equal __, MyAnimals::Bird.new.legs_in_bird
+    assert_equal 2, MyAnimals::Bird.new.legs_in_bird
   end
 
   # QUESTION: Which has precedence: The constant in the lexical scope,
   # or the constant from the inheritance hierarchy?
+
+  # The lexical scope.
 
   # ------------------------------------------------------------------
 
@@ -78,10 +80,34 @@ class AboutConstants < Neo::Koan
   end
 
   def test_who_wins_with_explicit_scoping_on_class_definition
-    assert_equal __, MyAnimals::Oyster.new.legs_in_oyster
+    assert_equal 4, MyAnimals::Oyster.new.legs_in_oyster
   end
 
   # QUESTION: Now which has precedence: The constant in the lexical
   # scope, or the constant from the inheritance hierarchy?  Why is it
   # different than the previous answer?
+
+=begin 
+
+When you define MyAnimals::Oyster you are still in the global scope, 
+so ruby has no knowledge of the LEGS value set to 2 in MyAnimals 
+because you never actually are in the scope of MyAnimals 
+(a little counterintuitive).
+
+However, things would be different if you were to define Oyster this way:
+
+class MyAnimals
+  class Oyster < Animal
+    def legs_in_oyster
+      LEGS # => 2
+    end
+  end
+end
+The difference is that in the code above, by the time you define Oyster, 
+you have dropped into the scope of MyAnimals, 
+so ruby knows that LEGS refers to MyAnimals::LEGS (2) and not Animal::LEGS (4).
+
+https://stackoverflow.com/questions/5464811/ruby-koans-explicit-scoping-on-a-class-definition-part-2
+
+=end
 end
