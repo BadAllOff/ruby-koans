b5dd332d08578b738bce9ab4155ef42c959cfcfd
Shamil <badalloffsh@gmail.com>
2017-11-09 12:30:36 +0100

    Add: triangle project 2

--- triangle.rb
+++ triangle.rb
@@ -14,9 +14,20 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  return :equilateral if a == b && b == c
-  return :isosceles if a == b || a == c || b == c
-  return :scalene if a != b || a != c || b != c	
+	if ( a <= 0 || b <= 0 || c <= 0 ) then
+		raise TriangleError, "Sides of triangle can't be less or equal to zero"
+	# if the sum of two sides less or egual to one side: it's not a triangle
+	elsif ( a >= b+c || b >= a+c || c >= a+b ) then
+		raise TriangleError, "This is not a triangle"
+	else
+	  if a == b && b == c then
+	  	:equilateral 
+	  elsif a == b || a == c || b == c then
+	  	:isosceles
+	  else
+	  	:scalene
+	  end
+	end
 end
 
 # Error class used in part 2.  No need to change this code.
